eb36a4b314482e4f4fb973ff725bcd51a1b5109d
Afimia Fidelis <38649892+Izebiafe@users.noreply.github.com>
2023-07-08 13:34:32 +0000

    update mobile view

---
+++ assets/js/projects-data.js
@@ -0,0 +1,77 @@
+const projectsData = [
+  {
+    id: '01',
+    name: 'Tonia',
+    role: 'Developer',
+    platform: 'Web',
+    year: '2022',
+    description:
+      'A daily selection of privately personalized reads; no accounts or sign-ups required. ',
+    descriptionlong:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent ',
+    featuredImage: 'assets/images/portfolio-1.svg',
+    technologies: ['HTML', 'CSS', 'ReactJs'],
+    linkToLiveVersion: '#',
+    linkToSource: '#',
+  },
+  {
+    id: '02',
+    name: 'Tonia 2',
+    role: 'Developer',
+    platform: 'Web',
+    year: '2022',
+    description:
+      'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    descriptionlong:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent.',
+    featuredImage: 'assets/images/portfolio-2.svg',
+    technologies: ['HTML', 'CSS', 'JavaScript', 'Ruby on rails'],
+    linkToLiveVersion: '#',
+    linkToSource: '#',
+  },
+  {
+    id: '03',
+    name: 'Tonia 3',
+    role: 'Developer',
+    platform: 'Web',
+    year: '2022',
+    description:
+      'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    descriptionlong:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent.',
+    featuredImage: 'assets/images/portfolio-3.svg',
+    technologies: ['HTML', 'CSS', 'JavaScript'],
+    linkToLiveVersion: '#',
+    linkToSource: '#',
+  },
+  {
+    id: '04',
+    name: 'Tonia 4',
+    role: 'Developer',
+    platform: 'Web',
+    year: '2022',
+    description:
+      'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    descriptionlong:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent.',
+    featuredImage: 'assets/images/portfolio-4.svg',
+    technologies: ['HTML', 'CSS', 'JavaScript'],
+    linkToLiveVersion: '#',
+    linkToSource: '#',
+  },
+  {
+    id: '05',
+    name: 'Todo List',
+    role: 'Developer',
+    platform: 'Web',
+    year: '2023',
+    description:
+      'To Do List is a simple HTML/CSS/JS app that allows users to create and manage their tasks in a digital format.',
+    descriptionlong:
+      'To Do List is a simple HTML/CSS/JS app that allows users to create and manage their tasks in a digital format. It is a great way to stay organized and on track with your work or personal life. It allows you to Add, edit, and delete tasks and Mark tasks as complete',
+    featuredImage: 'assets/images/todo.png',
+    technologies: ['HTML', 'CSS', 'JavaScript', 'Webpack'],
+    linkToLiveVersion: 'https://to-do-list-e19agzkqh-ticoniq.vercel.app',
+    linkToSource: 'https://github.com/ticoniq/To-do-list',
+  }];
+export default projectsData;
